0153d350619b509638e65617655a24613c3969d2
suman <suman7495@gmail.com>
2019-09-27 19:32:37 +0530

    Second update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='random',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT License',
+    author='kamal',
+    author_email='',
+    description=''
+)
